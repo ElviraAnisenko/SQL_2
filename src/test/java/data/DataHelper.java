7cb43c7406c9fb02a45914fdcc78d65e7b71f01f
Anisenko Elvira <elvira.anisenko@yandex.ru>
2023-12-28 22:50:49 +0300

    Initial commit

---
+++ src/test/java/data/DataHelper.java
@@ -0,0 +1,69 @@
+package data;
+
+
+import lombok.Value;
+
+import java.util.Random;
+
+
+public class DataHelper {
+    private DataHelper() {
+    }
+
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+
+    public static AuthInfo getUser() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+
+    @Value
+    public static class VerificationCode {
+        String login;
+        String code;
+    }
+
+    public static VerificationCode getVerification(String code) {
+        return new VerificationCode("vasya", code);
+    }
+
+    @Value
+    public static class Transfer {
+        String from;
+        String to;
+        Integer amount;
+    }
+
+    public static Transfer getTransfer(String from, String to, int amount) {
+        return new Transfer(from, to, amount);
+    }
+
+    public static int generateValidAmount(int balance) {
+        return new Random().nextInt(Math.abs(balance)) + 1;
+    }
+
+    public static int generateInValidAmount(int balance) {
+        return Math.abs(balance) + new Random().nextInt(10000);
+    }
+
+    public static Card getfirstcard() {
+        return new Card("92df3f1c-a033-48e6-8390-206f6b1f56c0", "5559 0000 0000 0001");
+    }
+
+    public static Card getsecondcard() {
+        return new Card("0f3f5c2a-249e-4c3d-8287-09f7a039391d", "5559 0000 0000 0002");
+    }
+
+    @Value
+    public static class Card {
+        String id;
+        String cardNumber;
+
+    }
+
+}
---
+++ src/test/java/test/ApiTest.java
@@ -0,0 +1,144 @@
+package test;
+
+import data.DataHelper;
+import data.SQLHelper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ApiTest {
+
+    @AfterAll
+    static void cleanDB () {
+        SQLHelper.cleanDatabase();
+    }
+
+    static String token;
+    static Integer firstCardBalance;
+    static Integer secondCardBalance;
+
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+
+    @Order(1)
+    @Test
+    public void requestForAuth() {
+        var user = DataHelper.getUser();
+        given()
+                .spec(requestSpec)
+                .body(user)
+                .when()
+                .post("/api/auth")
+                .then()
+                .statusCode(200);
+    }
+
+    @Order(2)
+    @Test
+    public void requestForVerification() {
+        var verificationCode = SQLHelper.getVerificationCode();
+        var verification = DataHelper.getVerification(verificationCode);
+        token =
+                given()
+                        .spec(requestSpec)
+                        .body(verification)
+                        .when()
+                        .post("/api/auth/verification")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .path("token");
+        assertThat(token, equalTo("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbiI6InZhc3lhIn0.JmhHh8NXwfqktXSFbzkPohUb90gnc3yZ9tiXa0uUpRY"));
+
+
+    }
+    @Order(3)
+    @Test
+    public void requestForViewingCard() {
+
+        requestForVerification();
+        Response response =
+                given()
+                        .spec(requestSpec)
+                        .header("Authorization", "Bearer " + token)
+                        .when()
+                        .get("/api/cards")
+                        .then()
+                        .body("[0].id", equalTo("0f3f5c2a-249e-4c3d-8287-09f7a039391d"))
+                        .body("[1].id", equalTo("92df3f1c-a033-48e6-8390-206f6b1f56c0"))
+                        .statusCode(200)
+                        .extract().response();
+        firstCardBalance = response.path("[1].balance");
+        secondCardBalance = response.path("[0].balance");
+    }
+
+    @Order(4)
+    @Test
+    public void requestForValidTransferMoney() {
+        requestForViewingCard();
+        var firstCard = DataHelper.getfirstcard().getCardNumber();
+        var secondCard = DataHelper.getsecondcard().getCardNumber();
+        int amount = DataHelper.generateValidAmount(firstCardBalance);
+        var transfer = DataHelper.getTransfer(firstCard, secondCard, amount);
+
+        given()
+                .spec(requestSpec)
+                .header("Authorization", "Bearer " + token)
+                .body(transfer)
+                .when()
+                .post("/api/transfer")
+                .then()
+                .statusCode(200);
+        int expectedBalanceFirstCard = firstCardBalance - amount;
+        int expectedBalanceSecondCard = secondCardBalance + amount;
+        requestForViewingCard();
+        assertThat(firstCardBalance, equalTo(expectedBalanceFirstCard));
+        assertThat(secondCardBalance, equalTo(expectedBalanceSecondCard));
+
+    }
+
+    @Order(5)
+    @Test
+    public void requestForInvalidTransferMoney() {
+        requestForViewingCard();
+        var firstCard = DataHelper.getfirstcard().getCardNumber();
+        var secondCard = DataHelper.getsecondcard().getCardNumber();
+        int amount = DataHelper.generateInValidAmount(firstCardBalance);
+        var transfer = DataHelper.getTransfer(firstCard, secondCard, amount);
+
+        given()
+                .spec(requestSpec)
+                .header("Authorization", "Bearer " + token)
+                .body(transfer)
+                .when()
+                .post("/api/transfer")
+                .then()
+                .statusCode(400);
+        int expectedBalanceFirstCard = firstCardBalance;
+        int expectedBalanceSecondCard = secondCardBalance;
+        requestForViewingCard();
+        //assertThat(firstCardBalance, equalTo(expectedBalanceFirstCard));
+        //assertThat(secondCardBalance, equalTo(expectedBalanceSecondCard));
+        assertAll(() -> assertThat(firstCardBalance, equalTo(expectedBalanceFirstCard)),
+                () -> assertThat(secondCardBalance, equalTo(expectedBalanceSecondCard)));
+
+    }
+
+
+}
